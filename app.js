1aff9d646fbd408f92822a17f1a16ebb53a15158
shivang-singh-mm <103564878+shivang-singh-mm@users.noreply.github.com>
2023-01-11 12:25:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(require('./post-stuff/post'))
 app.use(require('./profile/user'))
 app.use(require('./followers-following/route'))
 
-app.use(express.static(path.join(__dirname,"./note/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 app.get("*",function(_,res){
     res.sendFile(
-        path.join(__dirname,"./note/build/index.html"),
+        path.join(__dirname,"./client/build/index.html"),
         function(err){
             res.status(500).send(err);
         }
